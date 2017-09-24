e528886f3d0a4b7e2a837976b30bbaa7fcc1afbe
cyoung90 <chlckdud78@gmail.com>
2017-09-24 17:44:39 +0900

    1

--- service-worker.js
+++ service-worker.js
@@ -17,21 +17,21 @@ var cacheName = 'weatherPWA-final-1';
 var filesToCache = [
   '/',
   '/index.html',
-  '/scripts/app.js',
-  '/styles/inline.css',
-  '/images/clear.png',
-  '/images/cloudy-scattered-showers.png',
-  '/images/cloudy.png',
-  '/images/fog.png',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/partly-cloudy.png',
-  '/images/rain.png',
-  '/images/scattered-showers.png',
-  '/images/sleet.png',
-  '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
+  '/eNotisPlus2/scripts/app.js',
+  '/eNotisPlus2/styles/inline.css',
+  '/eNotisPlus2/images/clear.png',
+  '/eNotisPlus2/images/cloudy-scattered-showers.png',
+  '/eNotisPlus2/images/cloudy.png',
+  '/eNotisPlus2/images/fog.png',
+  '/eNotisPlus2/images/ic_add_white_24px.svg',
+  '/eNotisPlus2/images/ic_refresh_white_24px.svg',
+  '/eNotisPlus2/images/partly-cloudy.png',
+  '/eNotisPlus2/images/rain.png',
+  '/eNotisPlus2/images/scattered-showers.png',
+  '/eNotisPlus2/images/sleet.png',
+  '/eNotisPlus2/images/snow.png',
+  '/eNotisPlus2/images/thunderstorm.png',
+  '/eNotisPlus2/images/wind.png'
 ];
 
 self.addEventListener('install', function(e) {
